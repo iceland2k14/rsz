8719e056ede9fc28df505df8e748070adec99fd2
iceland <75991805+iceland2k14@users.noreply.github.com>
2021-12-07 14:18:31 +0530

    Script Upload

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # rsz
 Retrieve ECDSA signature R,S,Z values from blockchain rawtx or txid.
+
+## Info
+The script parse the data of rawtx to fetch all the inputs in the transaction and reconstructs the unsigned message for each of them
+ to find the Z value. The result is given as R,S,Z,Pubkey for each of the inputs present in the rawtx data. _**[No Internet required]**_  
+ 
+ If txid is given, instead of rawtx then blockchain API is used to fetch the details of rawtx and then R,S,Z calculation starts. _**[Internet required]**_
+
+## Usage: Python 3
+```python getz_input.py```
+
+## Run
+```
+usage: getz_input.py [-h] [-txid TXID] [-rawtx RAWTX]
+
+This tool helps to get ECDSA Signature r,s,z values from Bitcoin rawtx or txid
+
+optional arguments:
+  -h, --help    show this help message and exit
+  -txid TXID    txid of the transaction. Use Internet to fetch rawtx from
+                given txid
+  -rawtx RAWTX  Raw Transaction on the blockchain. No internet required
+
+Enjoy the program! :) Tips BTC: bc1q39meky2mn5qjq704zz0nnkl0v7kj4uz6r529at
+```
+
+```
+(base) C:\anaconda3\RSZ>python getz_input.py -txid 82e5e1689ee396c8416b94c86aed9f4fe793a0fa2fa729df4a8312a287bc2d5e
+
+Starting Program...
+======================================================================
+[Input Index #: 0]
+     R: 009bf436ce1f12979ff47b4671f16b06a71e74269005c19178384e9d267e50bbe9
+     S: 00c7eabd8cf796a78d8a7032f99105cdcb1ae75cd8b518ed4efe14247fb00c9622
+     Z: 9f4503ab6cae01b9fc124e40de9f3ec3cb7a794129aa3a5c2dfec3809f04c354
+PubKey: 04e3896e6cabfa05a332368443877d826efc7ace23019bd5c2bc7497f3711f009e873b1fcc03222f118a6ff696efa9ec9bb3678447aae159491c75468dcc245a6c
+======================================================================
+[Input Index #: 1]
+     R: 0094b12a2dd0f59b3b4b84e6db0eb4ba4460696a4f3abf5cc6e241bbdb08163b45
+     S: 07eaf632f320b5d9d58f1e8d186ccebabea93bad4a6a282a3c472393fe756bfb
+     Z: 94bbf25ba5b93ba78ee017eff80c986ee4e87804bee5770fae5b486f05608d95
+PubKey: 04e3896e6cabfa05a332368443877d826efc7ace23019bd5c2bc7497f3711f009e873b1fcc03222f118a6ff696efa9ec9bb3678447aae159491c75468dcc245a6c
+```
+
+```
+(base) C:\anaconda3\RSZ>python getz_input.py -rawtx 01000000028370ef64eb83519fd14f9d74826059b4ce00eae33b5473629486076c5b3bf215000000008c4930460221009bf436ce1f12979ff47b4671f16b06a71e74269005c19178384e9d267e50bbe9022100c7eabd8cf796a78d8a7032f99105cdcb1ae75cd8b518ed4efe14247fb00c9622014104e3896e6cabfa05a332368443877d826efc7ace23019bd5c2bc7497f3711f009e873b1fcc03222f118a6ff696efa9ec9bb3678447aae159491c75468dcc245a6cffffffffb0385cd9a933545628469aa1b7c151b85cc4a087760a300e855af079eacd25c5000000008b48304502210094b12a2dd0f59b3b4b84e6db0eb4ba4460696a4f3abf5cc6e241bbdb08163b45022007eaf632f320b5d9d58f1e8d186ccebabea93bad4a6a282a3c472393fe756bfb014104e3896e6cabfa05a332368443877d826efc7ace23019bd5c2bc7497f3711f009e873b1fcc03222f118a6ff696efa9ec9bb3678447aae159491c75468dcc245a6cffffffff01404b4c00000000001976a91402d8103ac969fe0b92ba04ca8007e729684031b088ac00000000
+
+Starting Program...
+======================================================================
+[Input Index #: 0]
+     R: 009bf436ce1f12979ff47b4671f16b06a71e74269005c19178384e9d267e50bbe9
+     S: 00c7eabd8cf796a78d8a7032f99105cdcb1ae75cd8b518ed4efe14247fb00c9622
+     Z: 9f4503ab6cae01b9fc124e40de9f3ec3cb7a794129aa3a5c2dfec3809f04c354
+PubKey: 04e3896e6cabfa05a332368443877d826efc7ace23019bd5c2bc7497f3711f009e873b1fcc03222f118a6ff696efa9ec9bb3678447aae159491c75468dcc245a6c
+======================================================================
+[Input Index #: 1]
+     R: 0094b12a2dd0f59b3b4b84e6db0eb4ba4460696a4f3abf5cc6e241bbdb08163b45
+     S: 07eaf632f320b5d9d58f1e8d186ccebabea93bad4a6a282a3c472393fe756bfb
+     Z: 94bbf25ba5b93ba78ee017eff80c986ee4e87804bee5770fae5b486f05608d95
+PubKey: 04e3896e6cabfa05a332368443877d826efc7ace23019bd5c2bc7497f3711f009e873b1fcc03222f118a6ff696efa9ec9bb3678447aae159491c75468dcc245a6c
+
+```
---
+++ getz_input.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: iceland
+"""
+import sys
+import hashlib
+import argparse
+from urllib.request import urlopen
+#==============================================================================
+parser = argparse.ArgumentParser(description='This tool helps to get ECDSA Signature r,s,z values from Bitcoin rawtx or txid', 
+                                 epilog='Enjoy the program! :)    Tips BTC: bc1q39meky2mn5qjq704zz0nnkl0v7kj4uz6r529at')
+
+parser.add_argument("-txid", help = "txid of the transaction. Automatically fetch rawtx from given txid", action="store")
+parser.add_argument("-rawtx", help = "Raw Transaction on the blockchain.", action="store")
+
+if len(sys.argv)==1:
+    parser.print_help()
+    sys.exit(1)
+args = parser.parse_args()
+#==============================================================================
+
+txid = args.txid if args.txid else ''
+rawtx = args.rawtx if args.rawtx else ''
+
+if rawtx == '' and txid == '': 
+    print('One of the required option missing -rawtx or -txid'); sys.exit(1)
+#==============================================================================
+
+def get_rs(sig):
+    rlen = int(sig[2:4], 16)
+    r = sig[4:4+rlen*2]
+#    slen = int(sig[6+rlen*2:8+rlen*2], 16)
+    s = sig[8+rlen*2:]
+    return r, s
+    
+def split_sig_pieces(script):
+    sigLen = int(script[2:4], 16)
+    sig = script[2+2:2+sigLen*2]
+    r, s = get_rs(sig[4:])
+    pubLen = int(script[4+sigLen*2:4+sigLen*2+2], 16)
+    pub = script[4+sigLen*2+2:]
+    assert(len(pub) == pubLen*2)
+    return r, s, pub
+
+
+# Returns list of this list [first, sig, pub, rest] for each input
+def parseTx(txn):
+    if len(txn) <130:
+        print('[WARNING] rawtx most likely incorrect. Please check..')
+        sys.exit(1)
+    inp_list = []
+    ver = txn[:8]
+    if txn[8:12] == '0001':
+        print('UnSupported Tx Input. Presence of Witness Data')
+        sys.exit(1)
+    inp_nu = int(txn[8:10], 16)
+    
+    first = txn[0:10]
+    cur = 10
+    for m in range(inp_nu):
+        prv_out = txn[cur:cur+64]
+        var0 = txn[cur+64:cur+64+8]
+        cur = cur+64+8
+        scriptLen = int(txn[cur:cur+2], 16)
+        script = txn[cur:2+cur+2*scriptLen] #8b included
+        r, s, pub = split_sig_pieces(script)
+        seq = txn[2+cur+2*scriptLen:10+cur+2*scriptLen]
+        inp_list.append([prv_out, var0, r, s, pub, seq])
+        cur = 10+cur+2*scriptLen
+    rest = txn[cur:]
+    return [first, inp_list, rest]
+
+#==============================================================================
+def get_rawtx_from_blockchain(txid):
+    try:
+        htmlfile = urlopen("https://blockchain.info/rawtx/%s?format=hex" % txid, timeout = 20)
+    except:
+        print('Unable to connect internet to fetch RawTx. Exiting..')
+        sys.exit(1)
+    else: res = htmlfile.read().decode('utf-8')
+    return res
+# =============================================================================
+
+def getSignableTxn(parsed):
+    res = []
+    first, inp_list, rest = parsed
+    tot = len(inp_list)
+    for one in range(tot):
+        e = first
+        for i in range(tot):
+            e += inp_list[i][0] # prev_txid
+            e += inp_list[i][1] # var0
+            if one == i: 
+                e += '1976a914' + HASH160(inp_list[one][4]) + '88ac'
+            else:
+                e += '00'
+            e += inp_list[i][5] # seq
+        e += rest + "01000000"
+        z = hashlib.sha256(hashlib.sha256(bytes.fromhex(e)).digest()).hexdigest()
+        res.append([inp_list[one][2], inp_list[one][3], z, inp_list[one][4], e])
+    return res
+#==============================================================================
+def HASH160(pubk_hex):
+    return hashlib.new('ripemd160', hashlib.sha256(bytes.fromhex(pubk_hex)).digest() ).hexdigest()
+#==============================================================================
+
+#txn = '01000000028370ef64eb83519fd14f9d74826059b4ce00eae33b5473629486076c5b3bf215000000008c4930460221009bf436ce1f12979ff47b4671f16b06a71e74269005c19178384e9d267e50bbe9022100c7eabd8cf796a78d8a7032f99105cdcb1ae75cd8b518ed4efe14247fb00c9622014104e3896e6cabfa05a332368443877d826efc7ace23019bd5c2bc7497f3711f009e873b1fcc03222f118a6ff696efa9ec9bb3678447aae159491c75468dcc245a6cffffffffb0385cd9a933545628469aa1b7c151b85cc4a087760a300e855af079eacd25c5000000008b48304502210094b12a2dd0f59b3b4b84e6db0eb4ba4460696a4f3abf5cc6e241bbdb08163b45022007eaf632f320b5d9d58f1e8d186ccebabea93bad4a6a282a3c472393fe756bfb014104e3896e6cabfa05a332368443877d826efc7ace23019bd5c2bc7497f3711f009e873b1fcc03222f118a6ff696efa9ec9bb3678447aae159491c75468dcc245a6cffffffff01404b4c00000000001976a91402d8103ac969fe0b92ba04ca8007e729684031b088ac00000000'
+if rawtx == '':
+    rawtx = get_rawtx_from_blockchain(txid)
+
+print('\nStarting Program...')
+
+m = parseTx(rawtx)
+e = getSignableTxn(m)
+
+for i in range(len(e)):
+    print('='*70,f'\n[Input Index #: {i}]\n     R: {e[i][0]}\n     S: {e[i][1]}\n     Z: {e[i][2]}\nPubKey: {e[i][3]}')
+
