4f30bf85c05ead480268df77cea2c64e6b71ed21
iceland <75991805+iceland2k14@users.noreply.github.com>
2024-07-14 15:33:52 +0530

    Update LLL using gmpy2
    Faster Solution using gmpy2

--- LLL_nonce_leakage.py
+++ LLL_nonce_leakage.py
@@ -1,27 +1,61 @@
 # -*- coding: utf-8 -*-
 """
-
 @author: iceland
-@credit: KV
+@credit: KV, pianist-coder
 """
-
-import olll
+###################################################################################
 import random
 import math
 import secp256k1 as ice
-
-G = ice.scalar_multiplication(1)
+import time
+from gmpy2 import mpq
+###################################################################################
 N = ice.N
-
-# Secret key = x = (rns1 – r1sn)-1 (snm1 – s1mn – s1sn(k1 – kn))
-# For most significant fix 128 bit leak at least 3 sig is required.
-fix_bits = 128
+fix_bits = 56
 out_file_name = 'pseudo_sig_rsz.txt'
-
 kbits = 256 - fix_bits
+###################################################################################
 
+def reduction(input_vectors, tolerance):
+    def gramschmidt_orthogonalization(vectors):
+        num_vectors = len(vectors)
+        basis = [None] * num_vectors
+        for i in range(num_vectors):
+            basis[i] = vectors[i].copy()
+            for j in range(i):
+                projection = sum(vectors[i][k] * basis[j][k] for k in range(len(vectors[i]))) / \
+                             sum(basis[j][k] * basis[j][k] for k in range(len(basis[j])))
+                for k in range(len(vectors[i])):
+                    basis[i][k] -= projection * basis[j][k]
+        return basis
 
-#==============================================================================
+    def update_orthogonal_vectors(orthogonalized, basis_vectors, index):
+        j = index - 1
+        while j >= 0:
+            mu_kj = sum(x * y for x, y in zip(orthogonalized[j], basis_vectors[index])) / sum(x ** 2 for x in orthogonalized[j])
+            if abs(mu_kj) > 0.5:
+                basis_vectors[index] = [v - round(mu_kj) * basis_vectors[j][k] for k, v in enumerate(basis_vectors[index])]
+            j -= 1
+        return basis_vectors
+
+    num_vectors = len(input_vectors)
+    basis_vectors = [None] * num_vectors
+    basis_vectors = [[mpq(x) for x in vec] for vec in input_vectors]
+    orthogonalized = gramschmidt_orthogonalization(basis_vectors)
+    index = 1
+    while index < num_vectors:
+        basis_vectors = update_orthogonal_vectors(orthogonalized, basis_vectors, index)
+        lhs_dot_lhs = sum(x ** 2 for x in orthogonalized[index - 1])
+        projection_coefficient = sum(x * y for x, y in zip(orthogonalized[index - 1], basis_vectors[index])) / lhs_dot_lhs
+        dot_product = sum(x ** 2 for x in orthogonalized[index])
+        if dot_product >= (tolerance - projection_coefficient ** 2) * lhs_dot_lhs:
+            index += 1
+        else:
+            basis_vectors[index], basis_vectors[index - 1] = basis_vectors[index - 1], basis_vectors[index]
+            orthogonalized = gramschmidt_orthogonalization(basis_vectors)
+            index = max(index - 1, 1)
+    return [[int(x) for x in b] for b in basis_vectors]
+
 def write_rsz_file(rr, ss, zz, pb):
     with open(out_file_name, 'w') as f:
         sz = len(rr)
@@ -37,81 +71,75 @@ def modinv(v):
 def getx(Q):
     return int(Q[1:33].hex(), 16)
 
-def minimum_sigs_required(num_bits):
+def minimum_sigs_required(num_bits: int) -> int:
     return math.ceil(1.03 * 4 / 3 * 256 / num_bits)
 
 def identity_plus2(u, elem=1):
-    m=[[0 for x in range(u+2)] for y in range(u)]
-    for i in range(0,u):
-        m[i][i] = elem
-    return m
-#==============================================================================
-n = 1 + minimum_sigs_required(fix_bits)
-print(f'\n Fixed Nonce bits = {fix_bits}           Minimum Signature Required = {n}')
+    result = [[0] * (u + 2) for _ in range(u)]
+    for i in range(u):
+        result[i][i] = elem
+    return result
 
+if __name__ == '__main__':
 
-# example secret = 0x7cf5d79d200207963474c64e64bde80ff4cb3e225b6ac5b6e958522fdab60578
-secret = random.randint(1,N)
-pub = ice.scalar_multiplication(secret)
-print('###############################################################################')
-print(f'secret: {hex(secret)}')
-print(f'Pubkey: {pub.hex()}')
-print('###############################################################################')
+    n = 1 + minimum_sigs_required(fix_bits)
+    print(f'\n Fixed Nonce bits = {fix_bits}           Minimum Signature Required = {n}')
+    secret = random.randint(1, N)
+    pub = ice.scalar_multiplication(secret)
+    print('###############################################################################')
+    print(f'secret: {hex(secret)[2:]}')
+    print(f'Pubkey: {pub.hex()}')
+    print('###############################################################################')
 
-fixed_k_prefix = random.randrange(2**kbits, N)
+    fixed_k_prefix = random.randrange(2**kbits, N)
+    z = [random.randrange(1, N) for i in range(n)]
+    nonces = [random.randrange(1, 2**kbits) + fixed_k_prefix for i in range(n)]
+    sigs_r = [getx(ice.scalar_multiplication(nonces[i])) for i in range(n)]
+    mod_inv_nonces = [modinv(nonces[i]) for i in range(n)]
+    sigs_s = [(z[i] + secret * sigs_r[i]) * mod_inv_nonces[i] % N for i in range(n)]
+    sinv = [modinv(s) for s in sigs_s]
+    write_rsz_file(sigs_r, sigs_s, z, pub)
+    matrix = identity_plus2(n - 1, N)
+    row, row2 = [], []
+    [zn, rn, sn] = [z[-1], sigs_r[-1], sigs_s[-1]]
+    rnsn_inv = rn * modinv(sn)
+    znsn_inv = zn * modinv(sn)
 
-#S = ((Z + (X * R)) / K) % N
-z = [random.randrange(1, N) for i in range(n)]
-nonces = [random.randrange(1, 2**kbits) + fixed_k_prefix for i in range(n)]
-sigs_r = [getx(ice.scalar_multiplication(nonces[i])) for i in range(n)]
-sigs_s = [( (z[i] + secret * sigs_r[i]) * modinv(nonces[i]) )%N for i in range(n)]
-sinv   = [modinv(sigs_s[i]) for i in range(n)]
+    for i in range(n-1):
+        row.append((sigs_r[i] * modinv(sigs_s[i])) - rnsn_inv)
+        row2.append((z[i] * modinv(sigs_s[i])) - znsn_inv)
 
-write_rsz_file(sigs_r, sigs_s, z, pub)
-###############################################################################
-matrix = identity_plus2(n-1, N)
+    row.append((2**kbits) / N)
+    row.append(0)
+    row2.append(0)
+    row2.append(2**kbits)
 
-# Add last 2 rows
-row, row2 = [], []
-[zn, rn, sn] = [z[-1], sigs_r[-1], sigs_s[-1]]
-rnsn_inv = rn * modinv(sn)
-znsn_inv = zn * modinv(sn)
+    matrix.append(row)
+    matrix.append(row2)
 
+    print(' Original Matrix')
+    print(matrix)
 
-# 2nd to last row: [r1(s1^-1) - rn(sn^-1), ... , rn-1(sn-1^-1) - rn(sn^-1), 2^176/order, 0 ]
-# last row: [m1(s1^-1) - mn(sn^-1), ... , mn-1(sn-1^-1) - mn(sn^-1), 0, 2^176]
-for i in range(n-1):
-    row.append((sigs_r[i] * modinv(sigs_s[i])) - rnsn_inv)
-    row2.append((z[i] * modinv(sigs_s[i])) - znsn_inv)
+    start = time.time()
+    new_matrix = reduction(matrix, 0.35)
 
-# add last elements of last two rows, B = 2**(256-80) for yubikey
-row.append((2**kbits) / N)
-row.append(0)
-row2.append(0)
-row2.append(2**kbits)
- 
-matrix.append(row)
-matrix.append(row2)
+    print('\n\n Reduced Matrix')
+    print(new_matrix)
+###################################################################################
+    for row in new_matrix:
+        potential_nonce_diff = row[0]
+        sigs_s_0 = sigs_s[0]
+        zn_times_sigs_s_0 = zn * sigs_s_0
+        sn_times_z_0 = sn * z[0]
+        sn_times_sigs_s_0_times_potential_nonce_diff = sn * sigs_s_0 * potential_nonce_diff
+        denominator = (rn * sigs_s_0) - (sigs_r[0] * sn)
+        denominator_inv = modinv(denominator)
+        potential_priv_key = (sn_times_z_0 - zn_times_sigs_s_0 - sn_times_sigs_s_0_times_potential_nonce_diff) * denominator_inv % N
 
-print(' Original Matrix')
-print(matrix)
-###############################################################################
-new_matrix = olll.reduction(matrix, 0.75)
-print('\n\n Reduced Matrix')
-print(new_matrix)
-
-for row in new_matrix:
-    potential_nonce_diff = row[0]
-#    print('k=', hex(potential_nonce_diff))
-#    modinv(r3 * s1 - r1 *s3) * (s3 * m1 - s1 * m3 - s1 * s3 *(k1 - k3))
-    potential_priv_key = (sn * z[0]) - (sigs_s[0] * zn) - (sigs_s[0] * sn * potential_nonce_diff)
-    potential_priv_key *= modinv((rn * sigs_s[0]) - (sigs_r[0] * sn))
-    potential_priv_key = potential_priv_key % N
-#    print('PK=', hex(potential_priv_key))
- 
-    # check if we found private key by comparing its public key with actual public key
-    if ice.scalar_multiplication(potential_priv_key) == pub:
-        print('-'*75)
-        print(f' found private key = {hex(potential_priv_key)}')
-        print('-'*75)
-###############################################################################
+        if ice.scalar_multiplication(potential_priv_key) == pub:
+            print('-'*79)
+            print(f' Found private key = {hex(potential_priv_key)[2:]}')
+            print('-'*79)
+###################################################################################
+    end = time.time()
+    print(f"time = {end - start:.2f} sec")
